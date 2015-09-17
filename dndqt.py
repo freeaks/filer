231561cf020e75bf7295a4977778a3bbfa433e76
nsipieter <nsipieter@purch.com>
2015-09-17 17:31:37 +0200

    scrollbars

--- dndqt.py
+++ dndqt.py
@@ -8,7 +8,7 @@
 #import draggableicons_rc
 
 
-from PyQt5.QtGui import * 
+from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import sys, os
@@ -107,46 +107,67 @@ class DragWidget(QFrame):
             child.setPixmap(pixmap)
 
 
+class Window(QWidget):
+    def __init__(self, path):
+        QWidget.__init__(self)
+        self.path = path
+        mainWidget = QWidget()
+        palette = QPalette()
+        palette.setBrush(QPalette.Background,QBrush(QPixmap("images/pattern.png")))
+        mainWidget.setPalette(palette)
+
+        scroll = QScrollArea()
+        scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        #scroll.setFrameShape(QFrame.NoFrame)
+        scroll.setWidget(mainWidget)
+        scroll.setWidgetResizable(True)
+        #scroll.setFixedHeight(100)
+        self.setStyleSheet("""
+            QScrollBar:vertical { border:none; width:6px } 
+            QScrollBar::handle:vertical { background: lightgray; }
+            QScrollBar::add-line:vertical { background: none; }
+            QScrollBar::sub-line:vertical { background: none; }            
+            """)
+     
+        # working:
+        layout = QVBoxLayout(self)
+        layout.addWidget(scroll)
+        myform = QHBoxLayout()
+        myform.addWidget(DragWidget())
+        mainWidget.setLayout(myform)
+        # -------
+
+        for name in os.listdir(path):
+            if os.path.isfile(os.path.join(path, name)):
+                print("file = ",name)
+                #DragImage('file.png',layout, path, name)
+            if os.path.isdir(os.path.join(path, name)):
+                print ("dire =", name)
+                #DragImage('directory.png',layout, path, name)
+
+#        palette = QPalette()
+#        palette.setBrush(QPalette.Background,QBrush(QPixmap("images/pattern.png")))
+#        mainWidget2 = QWidget()
+#        mainWidget2.setPalette(palette)
+#        horizontalLayout2 = QHBoxLayout()
+#        mainWidget2.setLayout(horizontalLayout2)
+#        horizontalLayout2.addWidget(DragWidget())
+#        mainWidget2.setWindowTitle("Draggable Iconsz")
+#        mainWidget2.show()
 
 
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    palette = QPalette()
-    palette.setBrush(QPalette.Background,QBrush(QPixmap("images/pattern.png")))
 
-    mainWidget = QWidget()
-    mainWidget.setPalette(palette)
-    horizontalLayout = QHBoxLayout()
-    
-    path = "./"
-    for name in os.listdir(path):
-        if os.path.isfile(os.path.join(path, name)):
-            print("file = ",name)
-            #DragImage('file.png',layout, path, name)
-        if os.path.isdir(os.path.join(path, name)):
-            print ("dire =", name)
-            #DragImage('directory.png',layout, path, name)
+    window = Window("./")
+    window.setGeometry(500, 100, 300, 100)
+    window.show()
 
-    
-    
-    horizontalLayout.addWidget(DragWidget())
-    mainWidget.setLayout(horizontalLayout)
-    mainWidget.setWindowTitle("Draggable Icons")
-
-
-
-
-    mainWidget.show()
-
-    mainWidget2 = QWidget()
-    mainWidget2.setPalette(palette)
-    horizontalLayout2 = QHBoxLayout()
-    mainWidget2.setLayout(horizontalLayout2)
-    horizontalLayout2.addWidget(DragWidget())
-    mainWidget2.setWindowTitle("Draggable Iconsz")
-    mainWidget2.show()
-
+    window2 = Window("./")
+    window2.setGeometry(100, 100, 300, 100)
+    window2.show()    
 
     sys.exit(app.exec_())
